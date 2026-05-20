650d60cc0f29cd73594436101fc215444e7cd095
Henry Darana <henry.darana@tray.com>
2026-05-20 15:21:18 +0530

    feat(frontend): add sort by release date and rating

--- src/main/java/com/streamingtracker/JustWatchClient.java
+++ src/main/java/com/streamingtracker/JustWatchClient.java
@@ -52,9 +52,9 @@ public class JustWatchClient {
         "          title" +
         "          originalReleaseYear" +
         "          runtime" +
+        "          originalLanguage" +
         "          ... on MovieOrShowContent {" +
         "            ageCertification" +
-        "            originalLanguage" +
         "          }" +
         "          genres {" +
         "            shortName" +
